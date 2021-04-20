178d69f92097f89b66fcd9b61391a642f5cff15b
refarwan <refarwanaan@gmail.com>
2021-04-20 08:25:55 +0700

    nyicil fungsi rfl.pesan di rfl.js, nyicil css pop-up di rfl.css dan menambah fungsi di skripjawa.js

---
+++ rfl/rfl.js
@@ -0,0 +1,17 @@
+let rfl = {};
+let hoverPenuh = document.createElement("div");
+hoverPenuh.classList.add("hover-penuh");
+rfl.pesan = function (pesan, tidakanLanjutan) {
+    let body = document.querySelector("body");
+    body.appendChild(hoverPenuh);
+    let popUp = document.createElement("div");
+    popUp.classList.add("pop-up");
+    let wadahPesan = document.createElement("div");
+    wadahPesan.innerHTML = pesan;
+    wadahPesan.classList.add("wadah-pesan");
+    popUp.appendChild(wadahPesan);
+    hoverPenuh.appendChild(popUp);
+    setTimeout(function () {
+        popUp.style.transform = "scale(1)";
+    }, 1)
+}
---
+++ skripjawa.js
@@ -0,0 +1,5 @@
+const tombolTesRflPesan1 = document.querySelector("button");
+
+tombolTesRflPesan1.onclick = function () {
+    rfl.pesan("Percobaan rfl.pesan 1");
+}
