c710abaf9e1fc9e190b059cb04f11c9ac30279eb
refarwan <refarwanaan@gmail.com>
2021-04-21 07:11:35 +0700

    menyelesaikan fungsi di rfl.js dan css di rfl.css

--- rfl/rfl.js
+++ rfl/rfl.js
@@ -1,14 +1,131 @@
-let rfl = {};
+let rf = {};
 let hoverPenuh = document.createElement("div");
 hoverPenuh.classList.add("hover-penuh");
-rfl.pesan = function (pesan, tidakanLanjutan) {
+
+rf.pesan = function (pesan, tidakanLanjutan) {
     let body = document.querySelector("body");
-    body.appendChild(hoverPenuh);
-    let popUp = document.createElement("div");
-    popUp.classList.add("pop-up");
-    let wadahPesan = document.createElement("div");
-    wadahPesan.innerHTML = pesan;
-    wadahPesan.classList.add("wadah-pesan");
-    popUp.appendChild(wadahPesan);
-    hoverPenuh.appendChild(popUp);
+    if (!body.contains(body.querySelector(".hover-penuh"))) {
+        body.appendChild(hoverPenuh);
+
+        let popUp = document.createElement("div");
+        popUp.classList.add("pop-up");
+
+        let wadahPesan = document.createElement("div");
+        wadahPesan.innerHTML = pesan;
+        wadahPesan.classList.add("wadah-pesan");
+        popUp.appendChild(wadahPesan);
+
+        let tombolOke = document.createElement("button");
+        tombolOke.innerHTML = "Oke";
+        tombolOke.classList.add("tombol-oke");
+        popUp.appendChild(tombolOke);
+        tombolOke.onclick = function () {
+            hoverPenuh.remove();
+            tombolOke.remove();
+            wadahPesan.remove();
+            popUp.remove();
+            if (tidakanLanjutan) tidakanLanjutan();
+        }
+        hoverPenuh.appendChild(popUp);
+        tombolOke.focus();
+    }
+
+}
+
+rf.konf = function (pesan, tindakanOke, tindakanBatal) {
+    let body = document.querySelector("body");
+    if (!body.contains(body.querySelector(".hover-penuh"))) {
+        body.appendChild(hoverPenuh);
+
+        let popUp = document.createElement("div");
+        popUp.classList.add("pop-up");
+
+        let wadahPesan = document.createElement("div");
+        wadahPesan.innerHTML = pesan;
+        wadahPesan.classList.add("wadah-pesan");
+        popUp.appendChild(wadahPesan);
+
+        let tombolOke = document.createElement("button");
+        tombolOke.innerHTML = "Oke";
+        tombolOke.classList.add("tombol-oke");
+        popUp.appendChild(tombolOke);
+
+        let tombolBatal = document.createElement("button");
+        tombolBatal.innerHTML = "Batal";
+        tombolBatal.classList.add("tombol-batal");
+        popUp.appendChild(tombolBatal);
+
+        function tutup() {
+            hoverPenuh.remove();
+            tombolOke.remove();
+            wadahPesan.remove();
+            popUp.remove();
+        }
+
+        tombolOke.onclick = function () {
+            tutup();
+            if (tindakanOke) tindakanOke();
+        }
+
+        tombolBatal.onclick = function () {
+            tutup();
+            if (tindakanBatal) tindakanBatal();
+        }
+        hoverPenuh.appendChild(popUp);
+        tombolOke.focus();
+    }
+}
+
+rf.isian = function (pesan, tindakanOke, tindakanBatal) {
+    let body = document.querySelector("body");
+    if (!body.contains(body.querySelector(".hover-penuh"))) {
+        body.appendChild(hoverPenuh);
+
+        let popUp = document.createElement("div");
+        popUp.classList.add("pop-up");
+
+        let wadahPesan = document.createElement("div");
+        wadahPesan.innerHTML = pesan;
+        wadahPesan.classList.add("wadah-pesan");
+        popUp.appendChild(wadahPesan);
+
+        let isian = document.createElement("input");
+        popUp.appendChild(isian);
+        isian.onkeyup = function (e) {
+            if (e.keyCode == 13) {
+                tombolOke.click();
+            }
+        }
+
+        let tombolOke = document.createElement("button");
+        tombolOke.innerHTML = "Oke";
+        tombolOke.classList.add("tombol-oke");
+        popUp.appendChild(tombolOke);
+
+        let tombolBatal = document.createElement("button");
+        tombolBatal.innerHTML = "Batal";
+        tombolBatal.classList.add("tombol-batal");
+        popUp.appendChild(tombolBatal);
+
+        function tutup() {
+            hoverPenuh.remove();
+            tombolOke.remove();
+            wadahPesan.remove();
+            popUp.remove();
+        }
+
+        tombolOke.onclick = function () {
+            tutup();
+            if (tindakanOke) tindakanOke(isian.value);
+        }
+
+        tombolBatal.onclick = function () {
+            tutup();
+            if (tindakanBatal) tindakanBatal(isian.value);
+        }
+        hoverPenuh.appendChild(popUp);
+        hoverPenuh.appendChild(popUp);
+        isian.focus();
+    }
+
 }
--- skripjawa.js
+++ skripjawa.js
@@ -1,5 +1,12 @@
-const tombolTesRflPesan1 = document.querySelector("button");
+const tombolTesRfPesan1 = document.querySelector("button");
 
-tombolTesRflPesan1.onclick = function () {
-    rfl.pesan("Percobaan rfl.pesan 1");
+tombolTesRfPesan1.onclick = function () {
+    rf.pesan("Percobaan rf.pesan 1");
+}
+
+const tombolTesRfPesan2 = document.querySelectorAll("button")[1];
+tombolTesRfPesan2.onclick = function () {
+    rf.pesan("Percobaan rf.pesan 2", function () {
+        alert("Tindakan Lanjutan")
+    });
 }
